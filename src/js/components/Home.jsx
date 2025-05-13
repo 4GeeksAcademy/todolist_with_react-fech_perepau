bda708bd812eec131da3d0869801290bc6d8748e
Pere Pau Carrasco Quesada <perepaucarrasco@gmail.com>
2025-05-13 23:59:37 +0000

    my first commit

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,103 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState, useEffect } from "react";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const [data, setData] = useState([])
+	const [task, setTask] = useState("")
+
+
+
+	useEffect(() => {
+		getTodos()
+
+	}, []);
+
+	const createUser = () => {
+		fetch('https://playground.4geeks.com/todo/users/carrasco', {
+			method: 'POST'
+		})
+			.then(resp => {
+				if (!resp.ok) throw new Error(`error code: ${resp.status}`)
+				return resp.json()
+			})
+			.then(data => getTodos())
+			.catch(err => console.log(err))
+
+	}
+
+	const getTodos = () => {
+		fetch('https://playground.4geeks.com/todo/users/carrasco')
+			.then(resp => {
+				if (!resp.ok) throw new Error(`error code: ${resp.status}`)
+				return resp.json()
+			})
+			.then(data => setData(data))
+			.catch(err => createUser())
+	}
+
+
+	const handleSubmit = e => {
+		e.preventDefault()
+		fetch('https://playground.4geeks.com/todo/todos/carrasco', {
+			method: 'POST',
+			body: JSON.stringify({
+				label: task, is_done: false
+			}),
+			headers: {
+				'content-Type': 'application/json'
+			}
+		})
+			.then(resp => {
+				if (!resp.ok) throw new Error(`error code: ${resp.status}`)
+				return resp.json()
+			})
+			.then(data => {
+				setTask("")
+				getTodos()
+			})
+			.catch(err => console.log(err))
+
+	}
+
+	const handleDelete = (id) => {
+		console.log(id)
+		fetch('https://playground.4geeks.com/todo/todos/' + id, {
+			method: 'DELETE',
+		})
+			.then(data => {
+				getTodos()
+			})
+
+	}
+
+	return (
+		<div className="container mt-5">
+			<h1 className="text-center mb-4">
+				My todo list!!!
+			</h1>
+				<form className="d-flex mb-3  justify-content-center align-items-center" onSubmit={handleSubmit}>
+					<input type="text" className="form-control me-2 text-center" placeholder="Que tarea necesitas hacer?" value={task} onChange={e => setTask(e.target.value)} />
+					<button type="submit" className="btn btn-primary">Añadir!</button>
+				</form>
+				<ul className="list-group">
+					{data.todos?.map(el=> (
+						<li className="d-flex list-group-item justify-content-between align-items-center" key={el.id}
+						>
+							{el.label}
+							<button className="btn btn-sm btn-outline-danger" onClick={() =>handleDelete (el.id)}>
+								&times;
+							</button>
+
+						</li>
+					))}
+
+				</ul>
 		</div>
-	);
+	)
+
+
+
 };
 
 export default Home;
